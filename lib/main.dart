e39b8f75d208be8313d67188bfe8e0c79673590f
Alef Luciano <alef.luciano@gmail.com>
2025-05-17 13:51:20 -0300

    Imagens | Informações dos produtos | Botão de adicionar

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,9 @@ class CarrinhoCompras extends StatelessWidget {
 class Produto {
   final String nome;
   final double preco;
-  final String imageUrl;
+  final String imagemUrl;
 
-  Produto(this.nome, this.preco, this.imageUrl);
+  Produto(this.nome, this.preco, this.imagemUrl);
 }
 
 class CarrinhoComprasPage extends StatefulWidget {
@@ -38,54 +38,54 @@ class CarrinhoComprasPage extends StatefulWidget {
 class _CarrinhoComprasPageState extends State<CarrinhoComprasPage> {
   final List<Produto> produtos = [
     Produto(
-      'banana',
+      'Banana',
       1.89,
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.naturaldaterra.com.br%2Fbanana-prata-kg%2Fp&psig=AOvVaw2Oj9jmfB3xw-03s0_Le3ec&ust=1747453629752000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCLppKKp40DFQAAAAAdAAAAABAE',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBb3xeGWriqXUAjoUZaNAnp0EeeVp0JSiLaw&s',
     ),
     Produto(
       'Morango',
       3.25,
-      'https://www.google.com/imgres?q=Morango&imgurl=https%3A%2F%2Fmercadoorganico.com%2F6423-large_default%2Fmorango-organico-250g-osm.jpg&imgrefurl=https%3A%2F%2Fmercadoorganico.com%2Fhortifruti-organico%2F4127-morango-organico-250g-osm-0618341542983.html&docid=ZHX9dtvsVwQU6M&tbnid=DeBm3DVGowD8eM&vet=12ahUKEwiBxdbAiqeNAxWkJrkGHVl_DNYQM3oECB0QAA..i&w=1000&h=1000&hcb=2&ved=2ahUKEwiBxdbAiqeNAxWkJrkGHVl_DNYQM3oECB0QAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-iS652xKs7iXonBJOuZUZNINeyIjBjXIrrQ&s',
     ),
     Produto(
       'Kiwi',
       2.10,
-      'https://www.google.com/imgres?q=Kiwi&imgurl=https%3A%2F%2Ftrela.com.br%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fprod-zapt-images.s3.sa-east-1.amazonaws.com%252Fproducts%252F842684d4-ad6e-4138-b4e2-f00b6f9cfdb6-kiwiverde-quitandatomio-12332.1.png%26w%3D3840%26q%3D75&imgrefurl=https%3A%2F%2Ftrela.com.br%2Fproduto%2Fkiwi-verde-500g-12332&docid=b0hQS9BKCizkZM&tbnid=M1q8PVFA2DEHFM&vet=12ahUKEwjPkKDYiqeNAxV6CrkGHa-6EkQQM3oECFsQAA..i&w=2400&h=2000&hcb=2&ved=2ahUKEwjPkKDYiqeNAxV6CrkGHa-6EkQQM3oECFsQAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiA-_xV7u2RlpOzmR7YsxtCjoeZ0TUWh8BLA&s',
     ),
     Produto(
       'Maça',
       1.99,
-      'https://www.google.com/imgres?q=ma%C3%A7a&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F1%2F15%2FRed_Apple.jpg%2F330px-Red_Apple.jpg&imgrefurl=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FMa%25C3%25A7%25C3%25A3&docid=nQ7-_pDQE8_h7M&tbnid=RGakCff1W1RNzM&vet=12ahUKEwjGh9r3iqeNAxU4FLkGHX5wAa8QM3oECB0QAA..i&w=330&h=299&hcb=2&ved=2ahUKEwjGh9r3iqeNAxU4FLkGHX5wAa8QM3oECB0QAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJMvzfVqQ8HJ4dUsAIetngNRI7KmIEFcsYVA&s',
     ),
     Produto(
       'Pêra',
       2.99,
-      'https://www.google.com/imgres?q=pera&imgurl=https%3A%2F%2Fwww.naturaldaterra.com.br%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fnaturalterra.vtexassets.com%252Farquivos%252Fids%252F163024%252FPera-Williams-Unidade.jpg%253Fv%253D638671093808830000%26w%3D320%26q%3D75&imgrefurl=https%3A%2F%2Fwww.naturaldaterra.com.br%2Fpera-williams-unidade%2Fp&docid=yRE1Uv4GRutFrM&tbnid=_LXl9Gy3-DKMxM&vet=12ahUKEwj2mb-Yi6eNAxUlCrkGHX8iFYcQM3oECBYQAA..i&w=320&h=320&hcb=2&ved=2ahUKEwj2mb-Yi6eNAxUlCrkGHX8iFYcQM3oECBYQAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4J8icV4S0BDYIQYSEwB4yom12DIan1AwNxg&s',
     ),
     Produto(
       'Manga',
       2.70,
-      'https://www.google.com/imgres?q=manga&imgurl=https%3A%2F%2Fstatic.mundoeducacao.uol.com.br%2Fmundoeducacao%2F2021%2F05%2Fmanga.jpg&imgrefurl=https%3A%2F%2Fmundoeducacao.uol.com.br%2Fbiologia%2Fmanga.htm&docid=1DcmwXVP4DtmgM&tbnid=N85vTKevDr_wkM&vet=12ahUKEwi92Ou3i6eNAxXKLLkGHVIVFp0QM3oECB0QAA..i&w=600&h=400&hcb=2&ved=2ahUKEwi92Ou3i6eNAxXKLLkGHVIVFp0QM3oECB0QAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzPUw6_bv0QcKNNfpO7_rq1KDHi5rPnBN-nw&s',
     ),
     Produto(
       'Goiaba',
       3.50,
-      'https://www.google.com/imgres?q=goiaba&imgurl=https%3A%2F%2Fs1.static.brasilescola.uol.com.br%2Fbe%2Fconteudo%2Fimages%2Ffruto-polpa-rosada-apta-para-consumo-537e5dda94063.jpg&imgrefurl=https%3A%2F%2Fbrasilescola.uol.com.br%2Ffrutas%2Fgoiaba.htm&docid=vqrXJ2LH8VT7HM&tbnid=qszNOKkyr9FgFM&vet=12ahUKEwimie_Qi6eNAxWtrpUCHclgAdMQM3oECG4QAA..i&w=300&h=201&hcb=2&ved=2ahUKEwimie_Qi6eNAxWtrpUCHclgAdMQM3oECG4QAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8BaJM7v7P2zV52toc-gZIQ790fuNYWhT6nQ&s',
     ),
     Produto(
       'Uva',
       1.99,
-      'https://www.google.com/imgres?q=uva&imgurl=https%3A%2F%2Fcdn.awsli.com.br%2F496%2F496853%2Fproduto%2F37100363%2Fmy-project-1---2023-05-13t182519-771-moh6ce2zg7.png&imgrefurl=https%3A%2F%2Fwww.dancruzplantas.com.br%2Fuva-francesa-preta-muda-enxertada&docid=_AkaDKKkAjFHkM&tbnid=YPRIcEDtBnpvEM&vet=12ahUKEwij9d7ni6eNAxWrrZUCHb7SDVAQM3oECBcQAA..i&w=1000&h=1000&hcb=2&ved=2ahUKEwij9d7ni6eNAxWrrZUCHb7SDVAQM3oECBcQAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgoFZbI6M4pN4afobM_fSfJjjpUfLa91sbiQ&s',
     ),
     Produto(
       'Ameixa',
       2.99,
-      'https://www.google.com/imgres?q=ameixa&imgurl=https%3A%2F%2Fcdn.awsli.com.br%2F600x450%2F18%2F18885%2Fproduto%2F1723476%2F27e1359cdf.jpg&imgrefurl=https%3A%2F%2Fwww.sementeorganica.com%2Fproduto%2Fmuda-de-ameixa-preta-enxertada-prunus-domestica.html&docid=QCiVUswcoQy5aM&tbnid=_Gy7XRZXerkq3M&vet=12ahUKEwiB48uejKeNAxV4qZUCHd1pBwIQM3oECBcQAA..i&w=600&h=450&hcb=2&ved=2ahUKEwiB48uejKeNAxV4qZUCHd1pBwIQM3oECBcQAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS392Y2nN44NTm-412HA29a19Po-RFKQ2eovg&s',
     ),
     Produto(
       'Amora',
       2.99,
-      'https://www.google.com/imgres?q=amora&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fcc%2FBlackberries_%2528Rubus_fruticosus%2529.jpg%2F250px-Blackberries_%2528Rubus_fruticosus%2529.jpg&imgrefurl=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FAmora&docid=o9yHkjC1XLNJSM&tbnid=C5cV4FrE5sCjcM&vet=12ahUKEwjfrPO8jKeNAxUkBLkGHdR3AO0QM3oECGUQAA..i&w=250&h=167&hcb=2&ved=2ahUKEwjfrPO8jKeNAxUkBLkGHdR3AO0QM3oECGUQAA',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj0nx8bOqS9EvgLVJKfX790ZZk4r8--HSHvg&s',
     ),
   ];
 
@@ -99,13 +99,67 @@ class _CarrinhoComprasPageState extends State<CarrinhoComprasPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'Carrinho de Compras',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          'Carrinho de Compras',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.builder(
+            itemCount: produtos.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+            ),
+            itemBuilder: (BuildContext context, index) {
+              var produto = produtos[index];
+              return Card(
+                elevation: 6,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Image.network(
+                        produto.imagemUrl,
+                        fit: BoxFit.fill,
+                        height: double.infinity,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(produto.nome),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('R\$${produto.preco.toStringAsFixed(2)}'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton.icon(
+                        label: Text('Adiconar'),
+                        onPressed: () {
+                          adicionarAoCarrinho(produto.preco);
+                        },
+                        icon: Icon(Icons.shopping_cart),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),
